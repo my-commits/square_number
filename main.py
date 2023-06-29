f23a0212cef4425188132ac96adb97ab9e3e56d4
my-commits <elornau@gmail.com>
2023-06-29 23:06:16 +0300

    used simple package

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from two_number_adder_my_commits.add_numbers import add_two_numbers
+
+
 def square_number(num):
     square = num ** 2
     return square
 
 
 if __name__ == '__main__':
+    num1 = int(input('Первое число: '))
+    num2 = int(input('Второе число: '))
+    print('Сумма', add_two_numbers(num1, num2))
     input_number = int(input("Введите число: "))
     result = square_number(input_number)
     print("Квадрат числа", input_number, "Равен", result)
